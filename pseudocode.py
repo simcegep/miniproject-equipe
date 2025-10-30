a946139074d5188a0dd44790e1c06484146518d2
info1 <6261485@cegepoutaouais.qc.ca>
2025-10-30 15:38:00 -0400

    1 début

---
+++ pseudocode.py
@@ -0,0 +1,25 @@
+#pseudocode
+# message ( Bienvenue au jeu de Sudoku)
+# choix de diff, facile, moyen, dure
+# créer grille
+# choix (boucle): rentrer un chiffre, enter pour afficher erreurs
+
+# afficher grille facile, moyen, dure
+grill = [
+ [0,]
+]
+
+#Personne A:crée la grille
+#Personne B:obtner les valeurs
+#Personne C:fait placer les valeurs
+
+#faire une Grille 9x9 level facile = tel nombre de chiffre affiche
+
+
+
+#
+#identifier fonction
+#prevoir date pseudo code ,fonction,test unitaire
+
+
+
