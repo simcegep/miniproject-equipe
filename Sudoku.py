5eb86b68392249860507c945a86a3873463cb36a
info1 <2389469@cegepoutaouais.qc.ca>
2025-11-03 10:50:58 -0500

    J'ai mis les grilles dans le code principale j'ai fait fonction valide J'ai fait la fonction placement chiffre

--- Sudoku.py
+++ Sudoku.py
@@ -1,5 +1,80 @@
-def choisir_difficulte():
 
+
+#fonction afficher la grille selon difficulté
+def choisir_difficulte():
+    print("1. Facile")
+    print("2. Moyen")
+    print("3. Difficile")
+    print("Choisissez une difficulté:")
+
+    choix_utilisateur = int(input(""))
+    if choix_utilisateur == 1:
+        print(grille_facile)
+        return grille_facile
+    elif choix_utilisateur == 2:
+        print(grille_moyen)
+        return grille_moyen
+    elif choix_utilisateur == 3:
+        print(grille_difficile)
+        return grille_difficile
+    else:
+        print("Choix invalide! Niveau facile est par défaut")
+        print(grille_facile)
+        return grille_facile
+
+# print (choisir_difficulte())
+
+def valide(grille, ligne, colonne, chiffre):
+    """
+
+    :param grille:
+    :param ligne:
+    :param colonne:
+    :param chiffre:
+    :return:
+    """
+
+    # vérifie la ligne
+    if chiffre in grille[ligne]:
+        return False
+
+    # vérifie la colonne
+    for i in range(9):
+        if grille[i][colonne] == chiffre:
+            return False
+
+    # Vérifie le carré 3x3
+    debut_ligne = (ligne // 3) * 3
+    debut_colonne = (colonne // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if grille[debut_ligne + i][debut_colonne + j] == chiffre:
+                return False
+    return True
+
+
+def placement_chiffres(grille, ligne, colonne, chiffre):
+   """
+
+   :param grille:
+   :param ligne:
+   :param colonne:
+   :param chiffre:
+   :return:
+   """
+   if grille[ligne][colonne] != "_":
+       print("La case est déjà remplie.")
+       return False
+   if valide(grille, ligne, colonne, chiffre):
+       grille[ligne][colonne] = chiffre
+       print("Le chiffre est placé avec succès! ")
+   else:
+       print("Le placement est invalide selon les règle du Sudoku. ")
+       return False
+
+
+
+if __name__ == '__main__':
     grille_facile = [
         [5, 3, "?", "?", 7, "?", "?", "?", "?"],
         [6, "?", "?", 1, 9, 5, "?", "?", "?"],
@@ -34,22 +109,3 @@ def choisir_difficulte():
         ["?", "?", 4, "?", "?", "?", "?", 3, "?"],
         ["?", "?", "?", "?", "?", 9, 7, "?", "?"]
     ]
-#fonction afficher la grille selon difficulté
-def choisir_difficulte():
-    print("Choisissez une difficulté:")
-    print("1. Facile")
-    print("2. Moyen")
-    print("3. Difficile")
-
-    choix_utilisateur = int(input(""))
-    if choix_utilisateur == 1:
-        return grille_facile
-    elif choix_utilisateur == 2:
-        return grille_moyen
-    elif choix_utilisateur == 3:
-        return grille_difficile
-    else:
-        print("Choix invalide, Niveau facile par défaut")
-        return grille_facile
-
-print (choisir_difficulte())
