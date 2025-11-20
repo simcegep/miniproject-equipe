c3db9e89ad05e26277ebce3674287a3f68db5448
marca <2389469@cegepoutaouais.qc.ca>
2025-11-20 18:57:01 -0500

    test unitaire

--- test_unitaire.py
+++ test_unitaire.py
@@ -7,70 +7,55 @@ def  test_modifier_case_valide():
     grille = [["_","_","_"],["_","_","_"],["_","_","_"]]
     grille_originale = [["_","_","_"],["_","_","_"],["_","_","_"]]
 
-    result = modifier_case(grille, grille_originale, 1,1,5)
-    result_attendu = grille[1][1] == "5"
+    modifier_case(grille, grille_originale, 1,1,5)
 
-    assert result == result_attendu
+
+    assert grille[1][1] == 5
 
 
 def  test_modifier_case_valide2():
     grille = [["_","_","_"],["_","3","_"],["_","_","_"]]
     grille_originale = [["_","_","_"],["_","_","_"],["_","_","_"]]
 
-    result = modifier_case(grille, grille_originale, 1,1,5)
-    result_attendu = grille[1][1] == "5"
+    modifier_case(grille, grille_originale, 1,1,5)
 
-    assert result == result_attendu
+
+    assert grille[1][1] == 5
 
 
 def test_modifier_case_interdite():
     grille = [["_", "_", "_"], ["_", "3", "_"], ["_", "_", "_"]]
     grille_originale = [["_", "_", "_"], ["_", "3", "_"], ["_", "_", "_"]]
 
-    result = modifier_case(grille, grille_originale, 1,1,7)
-    result_attendu = grille[1][1] == "3"
+    modifier_case(grille, grille_originale, 1,1,7)
 
-    assert result == result_attendu
+    assert grille[1][1] == "3"
 
 def test_modifier_case_invalide():
     grille = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     grille_originale = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
 
-    result = modifier_case(grille, grille_originale, 0,0,15)
-    result_attendu = grille[0][0] == "_"
+    modifier_case(grille, grille_originale, 0,0,15)
 
-    assert result == result_attendu
+    assert grille[0][0] == "_"
 
-
-
-def test_placement_chiffre_valide():
-    grille = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-    grille_originale = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-
-    result = placement_chiffres(grille, grille_originale, 1, 2, 4)
-    result_attendu = grille[1][2] == "4"
-
-    assert result == result_attendu
 
 
 def test_placement_chiffre_invalide():
     grille = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     grille_originale = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
 
-    result = placement_chiffres(grille, grille_originale, 0, 0, 15)
-    result_attendu = grille[0][0] == "_"
+    placement_chiffres(grille, 0, 0, 15, grille_originale)
 
-    assert result == result_attendu
-
+    assert grille[0][0] == "_"
 
 def test_placement_chiffre_invalide2():
-    grille = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-    grille_originale = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+    grille = [["_", "_", "_"], ["_", "3", "_"], ["_", "_", "_"]]
+    grille_solution = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
 
-    result = placement_chiffres(grille, grille_originale, 0, 0, 15)
-    result_attendu = grille[0][0] == "_"
+    placement_chiffres(grille, 0, 0, 5, grille_solution)
 
-    assert result == result_attendu
+    assert grille[0][0] == "_"
 
 
 #GABRIEL
